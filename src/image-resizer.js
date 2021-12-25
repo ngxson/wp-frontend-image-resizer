07a51ea53020d4938c51dd82a82717efde7c1ea0
ngxson <thichthat@gmail.com>
2021-12-25 19:00:21 +0100

    it works

--- src/image-resizer.js
+++ src/image-resizer.js
@@ -1,17 +1,40 @@
 import Resizer from 'react-image-file-resizer';
 
-window.resizeFile = (file) =>
-  new Promise((resolve) => {
+const resizeImageBeforeUpload = function (file, maxWidth, maxHeight, quality) {
+  const outputFormat = file.type === 'image/jpeg' ? 'JPEG'
+    : (file.type === 'image/png' ? 'PNG' : null);
+  if (!outputFormat) return Promise.resolve(file); // unsupported format
+  return new Promise((resolve) => {
     Resizer.imageFileResizer(
       file,
-      300,
-      300,
-      'JPEG',
-      100,
-      0,
-      (uri) => {
-        resolve(uri);
-      },
-      'base64'
+      maxWidth,
+      maxHeight,
+      outputFormat,
+      quality,
+      0, // rotate
+      (f) => resolve(f),
+      'file'
     );
   });
+}
+
+setTimeout(() => {
+  console.log('setup image resizer');
+  window.wp.apiFetch.use(async function (options, next) {
+    if (
+      options.path === '/wp/v2/media'
+      && options.method && options.method.toLowerCase() === 'post'
+      && options.body && options.body.get('file')
+    ) {
+      const originalFile = options.body.get('file');
+      const newFile = await resizeImageBeforeUpload(
+        originalFile,
+        window.FRONTEND_IMAGE_RESIZE.width,
+        window.FRONTEND_IMAGE_RESIZE.height,
+        window.FRONTEND_IMAGE_RESIZE.quality
+      );
+      options.body.set('file', newFile);
+    }
+    return next(options);
+  });
+}, 1000);
--- src/wp-frontend-image-resizer.php
+++ src/wp-frontend-image-resizer.php
@@ -10,13 +10,40 @@
  * Tested up to: 5.8
  */
 
+define('FRONTEND_IMAGE_RESIZE_MAX_WIDTH', 100);
+define('FRONTEND_IMAGE_RESIZE_MAX_HEIGHT', 100);
+define('FRONTEND_IMAGE_RESIZE_QUALITY', 80);
+
+function frontend_image_resizer_get_settings() {
+  return [
+    'enabled' => true,
+    'width' => FRONTEND_IMAGE_RESIZE_MAX_WIDTH,
+    'height' => FRONTEND_IMAGE_RESIZE_MAX_HEIGHT,
+    'quality' => FRONTEND_IMAGE_RESIZE_QUALITY,
+  ];
+}
+
+add_action('admin_head', function () {
+  ?><script>
+    window.FRONTEND_IMAGE_RESIZE = <?php echo json_encode(frontend_image_resizer_get_settings()); ?>;
+  </script><?php
+}, 100);
+
 function frontend_image_resizer_enqueue_script() {   
   wp_enqueue_script(
-    'frontend_image_resizer',
+    'frontend-image-resizer',
     plugin_dir_url(__FILE__) . 'image-resizer.min.js',
     array('jquery'),
-    '1.0'
+    '$version'
   );
 }
 
 add_action('admin_enqueue_scripts', 'frontend_image_resizer_enqueue_script');
+
+function frontend_image_resizer_plupload($params) {
+  $params['resize'] = frontend_image_resizer_get_settings();
+  return $params;
+}
+
+add_filter('plupload_init', 'frontend_image_resizer_plupload', 100);
+add_filter('plupload_default_settings', 'frontend_image_resizer_plupload', 100);
